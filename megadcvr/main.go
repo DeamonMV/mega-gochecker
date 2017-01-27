85772a56022b51d3f1086dbc12d8f5d5ec9fc419
Dmitriy Matveev <dimas_os@mail.ru>
2017-01-27 16:51:38 +0200

    updated megadcvr

--- megadcvr/main.go
+++ megadcvr/main.go
@@ -5,48 +5,19 @@ import (
 
 	"fmt"
 	"strings"
-	"strconv"
-
 	"io"
 	"os/exec"
 	"bytes"
 	"bufio"
 )
 
-func input(param string) bool {
-
-	param = strings.Replace(param, "\n","", -1)
-	if len(param) <= 3 && len(param) > 0 {
-		one := strings.Split(param, ";")
-
-		one1, err := strconv.Atoi(one[0])
-		//fmt.Printf("one1 %v\n", one1)
-		if err != nil {
-			fmt.Printf("%v", err)
-		}
-		one2, err := strconv.Atoi((one[1]))
-		//fmt.Printf("one2 %v\n", one2)
-		if err != nil {
-			fmt.Printf("%v", err)
-		}
-
-		if (one1 > 0) && (one1 <= 6) {
-			if (one2 == 0) || (one2 == 1) {
-				return true
-			}
-		}
-	}
-
-	return false
-
-}
 
 func main() {
 
 	slotkey := string("Slot Number")
 
-	c1 := exec.Command("cat", "data")
-	//c1 := exec.Command("megacli", "-PDList", "-aALL")
+	//c1 := exec.Command("cat", "data")
+	c1 := exec.Command("megacli", "-PDList", "-aALL")
 	c2 := exec.Command("egrep", "Enclosure Device ID:|Slot Number:|Inquiry Data:|Error Count:|state")
 
 	r, w := io.Pipe()
@@ -70,7 +41,6 @@ func main() {
 	for {
 
 		scanner, err := reader.ReadString('\n')
-		//	fmt.Println(scanner)
 
 		if err != nil {
 			if err == io.EOF {
@@ -83,20 +53,11 @@ func main() {
 		splited := strings.Split(strings.Replace(scanner, "\n", "", -1), ":")
 
 		if (splited[0] == slotkey)  {
+
 			buffer.WriteString(splited[1])
-			//fmt.Printf("%v\n", splited[1])
 			SlotCount = SlotCount + 1
-
 		}
-
-		//fmt.Println(scanner.Text())
-		//fmt.Println(rmspace)
-		//fmt.Println(splited[1])
 	}
-
-	//fmt.Println(buffer.String())
-	//fmt.Println(SlotCount)
-
 
 	SlotCountSplited := strings.Split((buffer.String())," ")
 	i := 1
@@ -107,7 +68,7 @@ func main() {
 			fmt.Println( "		{")
 			fmt.Printf("			\"{#SLOTNUMBER}\":\"%v\"}", SlotCountSplited[i])
 			if i < SlotCount  {
-				fmt.Println(",\n")
+				fmt.Println(",")
 			} else {
 				fmt.Println("]}")
 			}
